d7b70f148aeca2647ebf18da25769fdf45e2095a
Redvan <redvanisation@gmail.com>
2020-06-30 00:58:53 -0400

    Update comments post route

--- controllers/comments/comments.js
+++ controllers/comments/comments.js
@@ -1,7 +1,8 @@
 const handleWriteComment = (req, res, db) => {
-  const { user_id, post_id, body } = req.body;
+  const { user_id, body } = req.body;
+  const id = req.body.post_id;
 
-  if (!user_id || !post_id || !body) {
+  if (!user_id || !id || !body) {
     res.status(400).json('Invalid submission!');
   }
 
@@ -10,11 +11,23 @@ const handleWriteComment = (req, res, db) => {
     .insert({
       body: body,
       user_id: user_id,
-      post_id: post_id,
+      post_id: id,
       comment_created: new Date()
     })
-    .then(comment => res.json(comment[0]))
-    .catch(() => res.status(400).json('Unable to submit comment!'));
+    .then(() => {
+      db.select('*').from('posts').where({id})
+      .then(post => {
+        db('comments').join('users', 'comments.user_id', 'users.id').select('comments.id', 'comments.body', 'comments.comment_created', 'users.id', 'users.username', 'users.email', 'users.gravatar', 'users.blogger').where({'comments.post_id': id})
+        .then(data => {
+          res.json({
+            post: post[0],
+            comments: data
+          })
+        });
+      })
+      // .catch(() => res.status(400).json('Error getting the post!'));
+    })
+    .catch(() => res.status(400).json('Unable to add the comment!'));
 }
 
 module.exports = {
